e33c7eb895dc10dba88f36796ce44f22eed1148d
Caren Chang <carenc@google.com>
2021-06-15 10:31:24 -0700

    Add ugly edit text field

--- app/src/main/java/com/example/littlejoy/AddEventActivity.kt
+++ app/src/main/java/com/example/littlejoy/AddEventActivity.kt
@@ -1,11 +1,41 @@
 package com.example.littlejoy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.*
+import androidx.compose.material.Text
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.input.TextFieldValue
+import com.example.littlejoy.ui.theme.LittleJoyTheme
 
-class AddEventActivity : AppCompatActivity() {
+class AddEventActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_event)
+
+        setContent {
+            LittleJoyTheme {
+                editText()
+            }
+        }
     }
+
+    @Composable
+    fun editText() {
+        val textField = remember { mutableStateOf(TextFieldValue()) }
+
+        TextField(
+            value = textField.value,
+            onValueChange = {
+                textField.value  = it
+            },
+            placeholder = { Text(text = "Enter event name") }
+        )
+    }
+
+
 }
